73ed131d9a7bacd5be87225e3a697f93ddad13a9
Yajush Haarsh Rambhojun <43059398+HaarshR@users.noreply.github.com>
2020-10-27 21:05:08 +0400

    Added imports

---
+++ index.js
@@ -0,0 +1,14 @@
+// Imports
+const questionNumber = document.getElementById("questionNumber"); // Question Number
+const question = document.getElementById("question"); // Question
+
+// Radio buttons for answers
+const option1 = document.getElementById("option1");
+const option2 = document.getElementById("option2");
+const option3 = document.getElementById("option3");
+const option1 = document.getElementById("option4");
+
+// Buttons
+const btnNext = document.getElementById("btnNext");
+const btnPrevious = document.getElementById("btnPrevious");
+const btnSave = document.getElementById("btnSave");
